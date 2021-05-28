655498373b5ec79bc50829ffc546b033c6c7c854
Hayden Brown <scratchcat458@gmail.com>
2021-05-28 18:26:35 +1000

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ import pymongo
 client = pymongo.MongoClient("mongodb+srv://scrat:K5yUw2AWTTlH5ZHX@cluster0.tufcd.mongodb.net/myFirstDatabase"
                              "?retryWrites=true&w=majority")
 db = client.pycalc
+
+print("1 - Log in")
+print("2 - Sign up")
+print("3 - Use -demo")
+i = input("> ")
+
+if i == 1:
