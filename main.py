f7417ee70e2f7c07b4bfe3f91ecce513e15d91fc
Hayden Brown <scratchcat458@gmail.com>
2021-05-29 18:44:33 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pymongo
+import random
 
 client = pymongo.MongoClient("mongodb+srv://scrat:K5yUw2AWTTlH5ZHX@cluster0.tufcd.mongodb.net/myFirstDatabase"
                              "?retryWrites=true&w=majority")
@@ -6,7 +7,31 @@ db = client.pycalc
 
 print("1 - Log in")
 print("2 - Sign up")
-print("3 - Use -demo")
+print("3 - Use _demo")
 i = input("> ")
+print(i)
 
-if i == 1:
+if i == "1":
+    uName = input("Enter your username: ")
+    uId = input("Enter your uid: ")
+    x = db.users.find_one({"name": uName})
+    if x["UID"] == uId:
+        print("Auth successfull")
+    else:
+        print("Auth failed")
+        quit()
+elif i == "2":
+    print("Let's create your account :)")
+    uName = input("Enter your username: ")
+    uId = random.randint(100000,999999)
+    db.users.insert_one({"name": uName, "UID": uId})
+    print("Account created")
+    print("Your username will be: " + uName)
+    print("Your uid will be: " str(uId))
+elif i == "3":
+    uName = "_demo"
+    uId = 000000
+
+while true:
+    i = input(uName + "@PyCalc $ ")
+    
